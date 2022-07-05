3c0562f5e9115151207656d1b97511c67cde145f
Jaxson P <jaxpahu@gmail.com>
2022-07-04 19:36:21 -1000

    v1.1 improved error handling

--- spotify_logger.py
+++ spotify_logger.py
@@ -13,7 +13,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 import secrets
 
-bypass_device_filter = True
+bypass_device_filter = False
 
 class colors:
     reset='\033[0m'
@@ -60,194 +60,219 @@ def main ():
   spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-read-playback-state"))
   print(f"Successfully connected as {spotify.me()['display_name']}")
   
-  current_track_id = ""
 
-  print("Polling for new playback data...")
   while True:
+
     try:
-      playback_data = spotify.current_playback()
+      spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-read-playback-state"))
     except Exception as e:
-      print(f" {colors.fg.red}\n ! Encountered error while polling for spotify playback data:{colors.reset}")
+      print(f" {colors.fg.red}\n ! Encountered error while authorizing spotify:{colors.reset}")
       print(e)
       continue
 
-    #checking for new track
-    try:
-      if playback_data != None and current_track_id != playback_data['item']['id']:
+    current_track_id = ""
+    print("Polling for new song...")
 
-        print(f"{colors.fg.lightgreen} > Detected new track <{colors.reset}\nVerifying device...")
-        current_track_id = playback_data['item']['id']
+    while True:
 
-        if (playback_data['device']['name'] == 'Cafe TV' and playback_data['device']['type'] == 'TV') or bypass_device_filter:
-          print(f" - Device: {colors.fg.cyan}{playback_data['device']['name']} ({playback_data['device']['type']}){colors.reset}")
+      try:
+        playback_data = spotify.current_playback()
+      except Exception as e:
+        print(f" {colors.fg.red}\n ! Encountered error while polling for spotify playback data:{colors.reset}")
+        print(e)
+        break
 
+      #checking for new track
+      try:
+        if playback_data != None and current_track_id != playback_data['item']['id']:
 
-          print ("Gathering Made in Hope schedule data")
-          try:
-            mih_cal = str(requests.get(secrets.SCHEDULE_LINK).content)
-          except Exception as e:
-            print(f"{colors.fg.red} \n ! Encountered error while collecting MIH schedule data:{colors.reset}")
-            print(e)
-            current_track_id = ""
-            continue
-          
-          print("Parsing shifts...")
-          shifts = mih_cal.split("BEGIN:VEVENT")
-          current_employees = []
-          for shift in shifts:
-            if "SUMMARY" not in shift:
-              continue
-            raw_start = shift.split("DTSTART:")[1].split("\\")[0]
-            start = vDatetime.from_ical(raw_start).replace(tzinfo=None)
-            raw_end = shift.split("DTEND:")[1].split("\\")[0][:-1]
-            end = vDatetime.from_ical(raw_end).replace(tzinfo=None)
-            
-            now = datetime.utcnow()#datetime(2022, 7, 1, 2, 30, 0, )
-            
-            if now > start and now < end:
-              employee = " ".join(shift.split("SUMMARY:")[1].split(" ")[:2])
-              current_employees.append(employee)
+          print(f"{colors.fg.lightgreen} > Detected new track <{colors.reset}\nVerifying device...")
+          current_track_id = playback_data['item']['id']
 
-          print(f"{len(current_employees)} employee(s) currently on shift:")
-          [print(f" - {colors.fg.cyan}{employee}{colors.reset}") for employee in current_employees]
+          if (playback_data['device']['name'] == 'Cafe TV' and playback_data['device']['type'] == 'TV') or bypass_device_filter:
+            print(f" - Device: {colors.fg.cyan}{playback_data['device']['name']} ({playback_data['device']['type']}){colors.reset}")
 
 
-          print('Gathering track data...')
-          try:
-            track = spotify.track(current_track_id)
-          except Exception as e:
-            print(f"{colors.fg.red}\n ! Encountered error while collecting Spotify track data:{colors.reset}")
-            print(e)
-          
-          print('Gathering track features...')
-          try:
-            track_features = spotify.audio_features(current_track_id)[0]
-          except Exception as e:
-            print(f"{colors.fg.red} ! Encountered error while collecting Spotify track features:{colors.reset}")
-            print(e)
-          
-          print(f" - Track: {colors.fg.cyan}{track['name']} - {', '.join([artist['name'] for artist in track['artists']])} [{current_track_id}]{colors.reset}")
-          
-          print('Gathering artist genres')
-          all_genres = []
-          try:
-            for artist in track['artists']:
-              all_genres += spotify.artist(artist['id'])['genres']
-          except Exception as e:
-            print(f"{colors.fg.red} ! Encountered error while collecting artist genres:{colors.reset}")
-            print(e)
-
-          genres = []
-          for i in all_genres:
-              if i not in genres:
-                  genres.append(i)
-          
-          print('Gathering context data...')
-          try:
-            type = playback_data['context']['type']
-            print(f" - Type: {colors.fg.cyan}{type}{colors.reset}")
-            context_uri = playback_data['context']['uri'].split(":")[2]
-            if type == 'artist':
-              print('Gathering artist radio data')
-              context = spotify.artist(context_uri)
-              print(f" - Artist playlist: {colors.fg.cyan}{context['name']} [{context['id']}]{colors.reset}")
-            elif type == 'playlist':
-              print('Gathering playlist data')
-              context = spotify.playlist(context_uri)
-              print(f" - Playlist: {colors.fg.cyan}{context['name']} - {context['owner']['display_name']} [{context['id']}]{colors.reset}")
-            elif type == 'album':
-              print('Gathering album data')
-              context = spotify.playlist(context_uri)
-              print(f" - Album: {colors.fg.cyan}{context['name']} [{context['id']}]{colors.reset}")
-
-          except Exception as e:
-            print(f"{colors.fg.red}\n ! Encountered error while collecting Spotify context data:{colors.reset}")
-            print(e)
-            
-            current_track_id = ""
-            continue
-          
-          print("Aggregating data")
-          new_entry = []
-          with open('track_log.csv', 'r', encoding='utf-16') as f:
-            reader = csv.reader(f)
-            tracks = -1
-            for row in reader:
-              tracks += 1
-            new_entry.append(tracks)
-          
-          new_entry.append(str(datetime.utcnow()))
-          new_entry.append(", ".join(current_employees).replace("\\xca\\xbb", "\'"))
-          new_entry.append(track['name'])
-          new_entry.append(current_track_id)
-          new_entry.append(",".join([artist['name'] for artist in track['artists']]))
-          new_entry.append(playback_data['device']['volume_percent'])
-          new_entry.append(track['popularity'])
-          new_entry.append(track['explicit'])
-          new_entry.append(track_features['tempo'])
-          new_entry.append(track_features['duration_ms'])
-          new_entry.append(track_features['valence'])
-          new_entry.append(track_features['loudness'])
-          new_entry.append(track_features['energy'])
-          new_entry.append(track_features['liveness'])
-          new_entry.append(context['name'])
-          new_entry.append(context['id'])
-          new_entry.append(context['type'])
-          new_entry.append(",".join(genres))
-
-          print("Logging collected data")
-          with open('track_log.csv', 'a', newline='\n', encoding='utf-16') as f:
-            writer = csv.writer(f)
-            writer.writerow(new_entry)
-
-          print("Uploading data to cloud")
-          credentials = ServiceAccountCredentials.from_json_keyfile_name('google_client_secret.json', ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"])
-          client = gspread.authorize(credentials)
-          print("Authorized google sheets access\nWriting csv data to sheet")
-
-          upload_complete = False
-          while upload_complete != True:
-
+            print ("Gathering Made in Hope schedule data")
             try:
-              spreadsheet = client.open_by_key(secrets.SPREADSHEET_KEY)
-              worksheet = spreadsheet.worksheet("raw_data")
-              worksheet_rows = len(worksheet.col_values(1))
+              mih_cal = str(requests.get(secrets.SCHEDULE_LINK).content)
+            except Exception as e:
+              print(f"{colors.fg.red} \n ! Encountered error while collecting MIH schedule data:{colors.reset}")
+              print(e)
+              current_track_id = ""
+              break
+            
+            print("Parsing shifts...")
+            shifts = mih_cal.split("BEGIN:VEVENT")
+            current_employees = []
+            for shift in shifts:
+              if "SUMMARY" not in shift:
+                continue
+              raw_start = shift.split("DTSTART:")[1].split("\\")[0]
+              start = vDatetime.from_ical(raw_start).replace(tzinfo=None)
+              raw_end = shift.split("DTEND:")[1].split("\\")[0][:-1]
+              end = vDatetime.from_ical(raw_end).replace(tzinfo=None)
               
-              range = utils.rowcol_to_a1(worksheet_rows + 1, 1) + ":" + utils.rowcol_to_a1(worksheet_rows + 1, len(new_entry))
-              print(f' - Range: {colors.fg.cyan}{range}{colors.reset}')
-              cell_list = worksheet.range(range)
-              for cell in cell_list:
-                cell.value = new_entry[cell.col - 1]
-                print(f' - Uploading {colors.fg.cyan}{round(100 * (cell.col - 1) / len(new_entry), 2)}%{colors.reset}', end="\r")
-                time.sleep(0.1)
-              worksheet.update_cells(cell_list)
+              now = datetime.utcnow()#datetime(2022, 7, 1, 2, 30, 0, )
+              
+              if now > start and now < end:
+                employee = " ".join(shift.split("SUMMARY:")[1].split(" ")[:2])
+                current_employees.append(employee)
 
-              print(f' - Uploading {colors.fg.cyan}100.00%{colors.reset}')
+            print(f"{len(current_employees)} employee(s) currently on shift:")
+            [print(f" - {colors.fg.cyan}{employee}{colors.reset}") for employee in current_employees]
 
-              upload_complete = True
+
+            print('Gathering track data...')
+            try:
+              track = spotify.track(current_track_id)
+            except Exception as e:
+              print(f"{colors.fg.red}\n ! Encountered error while collecting Spotify track data:{colors.reset}")
+              print(e)
+            
+            print('Gathering track features...')
+            try:
+              track_features = spotify.audio_features(current_track_id)[0]
+            except Exception as e:
+              print(f"{colors.fg.red} ! Encountered error while collecting Spotify track features:{colors.reset}")
+              print(e)
+            
+            print(f" - Track: {colors.fg.cyan}{track['name']} - {', '.join([artist['name'] for artist in track['artists']])} [{current_track_id}]{colors.reset}")
+            
+            print('Gathering artist genres')
+            all_genres = []
+            try:
+              for artist in track['artists']:
+                all_genres += spotify.artist(artist['id'])['genres']
+            except Exception as e:
+              print(f"{colors.fg.red} ! Encountered error while collecting artist genres:{colors.reset}")
+              print(e)
+              break
+
+            genres = []
+            for i in all_genres:
+                if i not in genres:
+                    genres.append(i)
+            
+            print('Gathering context data...')
+            try:
+              type = playback_data['context']['type']
+              print(f" - Type: {colors.fg.cyan}{type}{colors.reset}")
+              context_uri = playback_data['context']['uri'].split(":")[2]
+              if type == 'artist':
+                print('Gathering artist radio data')
+                context = spotify.artist(context_uri)
+                print(f" - Artist playlist: {colors.fg.cyan}{context['name']} [{context['id']}]{colors.reset}")
+              elif type == 'playlist':
+                print('Gathering playlist data')
+                context = spotify.playlist(context_uri)
+                print(f" - Playlist: {colors.fg.cyan}{context['name']} - {context['owner']['display_name']} [{context['id']}]{colors.reset}")
+              elif type == 'album':
+                print('Gathering album data')
+                context = spotify.playlist(context_uri)
+                print(f" - Album: {colors.fg.cyan}{context['name']} [{context['id']}]{colors.reset}")
 
             except Exception as e:
-              print(f"\n{colors.fg.red}\n ! Encountered error while writing data to Google sheets:{colors.reset}")
+              print(f"{colors.fg.red}\n ! Encountered error while collecting Spotify context data:{colors.reset}")
               print(e)
-              print('\nTrying again...')
+              
+              current_track_id = ""
+              break
+            
+            print("Aggregating data")
+            new_entry = []
+            with open('track_log.csv', 'r', encoding='utf-16') as f:
+              reader = csv.reader(f)
+              tracks = -1
+              for row in reader:
+                tracks += 1
+              new_entry.append(tracks)
+            
+            new_entry.append(str(datetime.utcnow()))
+            new_entry.append(", ".join(current_employees).replace("\\xca\\xbb", "\'"))
+            new_entry.append(track['name'])
+            new_entry.append(current_track_id)
+            new_entry.append(",".join([artist['name'] for artist in track['artists']]))
+            new_entry.append(playback_data['device']['volume_percent'])
+            new_entry.append(track['popularity'])
+            new_entry.append(track['explicit'])
+            new_entry.append(track_features['tempo'])
+            new_entry.append(track_features['duration_ms'])
+            new_entry.append(track_features['valence'])
+            new_entry.append(track_features['loudness'])
+            new_entry.append(track_features['energy'])
+            new_entry.append(track_features['liveness'])
+            new_entry.append(context['name'])
+            new_entry.append(context['id'])
+            new_entry.append(context['type'])
+            new_entry.append(",".join(genres))
+
+            print("Logging collected data")
+            with open('track_log.csv', 'a', newline='\n', encoding='utf-16') as f:
+              writer = csv.writer(f)
+              writer.writerow(new_entry)
+
+            print("Uploading data to cloud")
+            credentials = ServiceAccountCredentials.from_json_keyfile_name('google_client_secret.json', ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"])
+            client = gspread.authorize(credentials)
+            print("Authorized google sheets access\nWriting csv data to sheet")
+
+            upload_complete = False
+            while upload_complete != True:
+
+              try:
+                spreadsheet = client.open_by_key(secrets.SPREADSHEET_KEY)
+                print("Found spreadsheet")
+                worksheet = spreadsheet.worksheet("raw_data")
+                print("Found worksheet")
+                worksheet_rows = len(worksheet.col_values(1))
+                
+                range = utils.rowcol_to_a1(worksheet_rows + 1, 1) + ":" + utils.rowcol_to_a1(worksheet_rows + 1, len(new_entry))
+                print(f' - Range: {colors.fg.cyan}{range}{colors.reset}')
+                cell_list = worksheet.range(range)
+                for cell in cell_list:
+                  cell.value = new_entry[cell.col - 1]
+                  print(f' - Uploading {colors.fg.cyan}{round(100 * (cell.col - 1) / len(new_entry), 2)}%{colors.reset}', end="\r")
+                  time.sleep(0.1)
+                worksheet.update_cells(cell_list)
+
+                print(f' - Uploading {colors.fg.cyan}100.00%{colors.reset}')
+
+                upload_complete = True
+
+              except Exception as e:
+                print(f"\n{colors.fg.red}\n ! Encountered error while writing data to Google sheets:{colors.reset}")
+                print(e)
+                print('\nTrying again...')
+            
+            print("Done!\nPolling for new playback data...")
+            time.sleep(15)
+
           
-          print("Done!\nPolling for new playback data...")
-          time.sleep(15)
+          else:
+            print(f"{colors.fg.red}Listening on Invalid device: {playback_data['device']['name']} ({playback_data['device']['type']}){colors.reset}")
+            print("Polling for new playback data...")
 
-        
-        else:
-          print(f"{colors.fg.red}Listening on Invalid device: {playback_data['device']['name']} ({playback_data['device']['type']}){colors.reset}")
-          print("Polling for new playback data...")
+      except Exception as e:
+        print(f"\n{colors.fg.red}\n ! Encountered unhandled error during excecution:{colors.reset}")
+        print(e)
+        break
 
-    except Exception as e:
-      print(f"\n{colors.fg.red}\n ! Encountered unhandled error during excecution:{colors.reset}")
-      print(e)
+      time.sleep(30)
+    print("Refreshing program loop")
 
-    time.sleep(30)
-
+  print("!!!!Broke program") 
   return
-
 
 if __name__ == '__main__':
   print('\n\n\n')
   main()
+
+
+
+
+
+
+
+
+
